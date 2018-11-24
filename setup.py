6d57beea8f0135fa5b8a10ebef82816e174b6953
jiho2007 <43025513+jiho2007@users.noreply.github.com>
2018-11-24 16:13:58 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mfrac",
+    version="0.0.1",
+    author="jiho2007",
+    author_email="zzang-9@naver.com",
+    description="A Small Fraction Class Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jiho2007/fraction",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
